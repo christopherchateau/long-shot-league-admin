b788bb2f2350f34c16f2c8157ce36f97b83b6055
chris <christopherchateau@gmail.com>
2019-03-26 11:55:20 -0600

    Add teamData and playerData to state in MainPage. Create apiCalls dir and index.js, write getTeamData and getPlayerData func's

---
+++ src/components/MainPage/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./MainPage.css";
+
+class MainPage extends Component {
+  state = {
+    teamData: [],
+    playerData: []
+  };
+
+  componentDidMount = async () => {
+
+    // this.setState({ teamData });
+  };
+
+
+
+  render() {
+    return <div className="MainPage" />;
+  }
+}
+
+export default MainPage;
