dac30192ac2028663edf96669d4508deb3134122
chris <christopherchateau@gmail.com>
2019-03-27 12:25:39 -0600

    Updates

---
+++ precache-manifest.4986cedb7294ac73ea47e6205d33b01b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "401918f05dbe47eb25e377818b092116",
+    "url": "/long-shot-league-admin/static/media/todd.401918f0.png"
+  },
+  {
+    "revision": "58e6fc112b41c0948b77a1fdad30e50c",
+    "url": "/long-shot-league-admin/static/media/court.58e6fc11.jpg"
+  },
+  {
+    "revision": "0ce75e9dc1cdd1859998",
+    "url": "/long-shot-league-admin/static/js/runtime~main.0ec1a9df.js"
+  },
+  {
+    "revision": "e3da56705a542d2c29e1",
+    "url": "/long-shot-league-admin/static/js/main.5a32ab9c.chunk.js"
+  },
+  {
+    "revision": "94a30ddd84e38845c67b",
+    "url": "/long-shot-league-admin/static/js/2.cc2b2045.chunk.js"
+  },
+  {
+    "revision": "e3da56705a542d2c29e1",
+    "url": "/long-shot-league-admin/static/css/main.10fc89cd.chunk.css"
+  },
+  {
+    "revision": "c81a811752e5b07a19d80923849f5b6e",
+    "url": "/long-shot-league-admin/index.html"
+  }
+];
