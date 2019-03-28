e0c91b3595e2415a5dbcd61747fe63e5973596c2
chris <christopherchateau@gmail.com>
2019-03-28 17:04:43 -0600

    Add logic to Players to sum player's bonus total. Import Bonus compnent to display individually

--- src/components/Players/index.js
+++ src/components/Players/index.js
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
+import Bonus from "../Bonus";
 import PlayerForm from "../PlayerForm";
 import "./Players.css";
 
 class Players extends Component {
   render() {
-    const { playerData, loadData } = this.props;
+    const { playerData, bonusData, loadData } = this.props;
 
-    const players = this.props.playerData.slice(1).map(player => (
-      <div className="player" key={player.name}>
-        <h3>{player.name}</h3>
-        <h5>bonus: {player.bonus_points}</h5>
-      </div>
-    ));
+    const players = this.props.playerData.slice(1).map(player => {
+      const bonusDataDisplayed = [];
+
+      const playerBonusTotal = bonusData.reduce((total, bonus) => {
+        if (bonus.name === player.name) {
+          bonusDataDisplayed.push(<Bonus bonusData={bonus} />);
+          total += bonus.points;
+        }
+        return +total;
+      }, 0);
+      return (
+        <div className="player" key={player.name}>
+          <h3>{player.name}</h3>
+          <h5>bonus: {playerBonusTotal}</h5>
+        </div>
+      );
+    });
     return (
       <div>
         <PlayerForm playerData={playerData} loadData={loadData} />
