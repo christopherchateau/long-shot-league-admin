80e088a2ecb04ea1c0166c1bd574ead551b65265
chris <christopherchateau@gmail.com>
2019-03-27 22:08:10 -0600

    Create Players component

---
+++ src/components/Players/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Players.css";
+
+class Players extends Component {
+  render() {
+    return (
+      <div className="Players">
+      </div>
+    );
+  }
+}
+
+export default Players;
