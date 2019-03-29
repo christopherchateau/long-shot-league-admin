9ac8bef39772c3ad993c956ca69bd87bb5b8e4b5
chris <christopherchateau@gmail.com>
2019-03-29 00:15:04 -0600

    Add team toggle button

--- src/components/Teams/index.js
+++ src/components/Teams/index.js
@@ -3,8 +3,24 @@ import TeamForm from "../TeamForm";
 import "./Teams.css";
 
 class Teams extends Component {
+  state = {
+    display: "show all"
+  };
+
+  toggleTeamDisplay = () => {
+    let { display } = this.state;
+    display === "show all" ? (display = "still alive") : (display = "show all");
+    this.setState({ display });
+  };
+
   render() {
-    const { teamData, loadData } = this.props;
+    const { display } = this.state;
+    let { teamData, loadData } = this.props;
+
+    if (display === "still alive") {
+      teamData = teamData.filter(team => !team.is_eliminated);
+    }
+
     const teams = teamData.slice(1).map(team => (
       <div
         className={"team".concat(team.is_eliminated ? " red" : " green")}
@@ -18,7 +34,11 @@ class Teams extends Component {
     ));
     return (
       <div>
-        <TeamForm teamData={teamData} loadData={loadData}/>
+        <TeamForm teamData={teamData} loadData={loadData} />
+        <button className="teams-toggle-btn" onClick={this.toggleTeamDisplay}>
+          {display}
+        </button>
+        <br />
         <div className="Teams">{teams}</div>;
       </div>
     );
