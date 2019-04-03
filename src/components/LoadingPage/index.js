e2c7ad1aba26c7b84f82970d8e4d9999b2cd9547
chris <christopherchateau@gmail.com>
2019-04-03 14:39:06 -0600

    Create LoadingPage component and css file

---
+++ src/components/LoadingPage/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import loadingImg from "../../images/loading.gif";
+import "./LoadingPage.css";
+
+class LoadingPage extends Component {
+  render() {
+    return (
+      <div className="LoadingPage">
+asdf
+      </div>
+    );
+  }
+}
+
+export default LoadingPage;
