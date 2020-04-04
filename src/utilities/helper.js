039c7c564b911c6c679ba44357fad46595e2e5a7
chris <christopherchateau@gmail.com>
2020-04-03 22:37:49 -0600

    refactor formatData in helper

--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -1,11 +1,15 @@
 export const validatePtsInput = input => !input.toString().length || +input < 0
 
 export const formatData = (types, data) =>
-	data.reduce((acc, d, i) => {
-		d.error
-			? acc.errors.push(d.error)
-			: acc[`${types[i]}Data`] = sortByName(d)
-		return acc
-	}, { errors: [] })
+	data.reduce(
+		(acc, d, i) => {
+			d.error
+				? acc.errors.push(d.error)
+				: (acc[`${types[i]}Data`] = sortByName(d))
+
+			return acc
+		},
+		{ errors: [] }
+	)
 
 const sortByName = input => input.sort((a, b) => (a.name < b.name ? -1 : 1))
